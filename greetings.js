9ba217e129ee9daa70df607b8f9dc7ca0db2ba34
bugg <buggbuggbuggg2@gmail.com>
2026-03-06 12:26:28 -0500

    Create greetings.js

---
+++ greetings.js
@@ -0,0 +1,33 @@
+//this is me learning how to use the console log, and strings. ezpz. 
+
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+//define bot, give it a string for its name 
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+//My name is teacherBot.
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+//I live in the universe. 
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+//change the string for bot
+bot = "professorBot";
+//My nickname is professorBot
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+//change bot string
+bot = "awesomeTeacherBot";
+//i love my nickname but wish people would call me awesomeTeacherBot
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubject = "Computer Science";
+//my favorite subject is computer science 
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+//sybau teacherBot

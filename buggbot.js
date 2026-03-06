c525cb4990db0a519683673ca959374b5d14b5f2
bugg <buggbuggbuggg2@gmail.com>
2026-03-06 12:38:20 -0500

    Create buggbot.js

---
+++ buggbot.js
@@ -0,0 +1,30 @@
+//buggbot
+//i know  i leave lots of comments, which isn't clean code, but clean code is for stupid elitist losers. im learning and it helps so shut up
+console.log("Hello! I'm your coding fun fact guide!");
+//hello!
+let botName;
+botName = "buggbot";
+
+let botLocation;
+botLocation = "ur computer!";
+// my name is buggbot and i live on ur computer
+console.log("My name is " + botName + " and I live on " + botLocation);
+
+let favoriteLanguage;
+favoriteLanguage = "HTML ";
+//my favorite language is html
+console.log("My favorite language is " + (favoriteLanguage) +".");
+
+console.log(codingFact + ".");
+//html is a markdown language I used to build ClubBugg
+let codingFact;
+codingFact = (favoriteLanguage + "is a markdown language I used to build ClubBugg");
+//html is not hard at all to learn.
+codingFact = ((favoriteLanguage) + "is not hard at all to learn.");
+
+console.log(codingFact);
+// html is easy peasy. 
+codingFact = (favoriteLanguage + "is easy peasy.");
+console.log(codingFact);
+//bye bye!
+console.log("It was fun  these facts with you. Goodbye! - " + (botName) + " from " + (botLocation));
